e69ed3589fe69b027bc7495ba10135078cbcbf25
kukhariev <q_h@inbox.ru>
2018-04-22 19:44:34 +0300

    test: ffmpeg filter as test source

--- test/ffprogress.spec.ts
+++ test/ffprogress.spec.ts
@@ -1,31 +1,54 @@
-const FFMPEG_PATH = process.env.FFMPEG_PATH || 'ffmpeg';
+import * as os from 'os';
+import * as ffmpeg from 'ffmpeg-static';
+const FFMPEG_PATH = ffmpeg.path;
+const tmp = os.tmpdir();
 import { expect } from 'chai';
 import { spawn } from 'child_process';
-import {
-  FFMpegProgress,
-  FFMpegProgressData
-} from '../src/';
-const testFile = './test/SampleVideo_640x360_1mb.flv';
-const args = [
-  '-re',
+import { FFMpegProgress, FFMpegProgressData } from '../src/';
+import { before } from 'mocha';
+
+const args0 = [
+  '-y',
+  '-f',
+  'lavfi',
+  '-i',
+  'testsrc=duration=10:size=640x360:rate=50',
+  `${tmp}/testfile.mp4`
+];
+
+const args1 = [
   '-y',
   '-i',
-  './test/SampleVideo 640x360 1mb.flv',
+  `${tmp}/testfile.mp4`,
   '-r',
-  '30',
+  '25',
   '-f',
   'mp4',
-  '/tmp/o.mp4'
+  `${tmp}/o.mp4`
 ];
-const errargs = ['-y', '-i', 'badfile', '-f', 'mp4', '/tmp/o.mp4'];
+const arg3 = ['-y', '-i', 'badfile', '-f', 'mp4', `${tmp}/o.mp4`];
+
 describe('ffmpeg-progress', () => {
-  it('should be able to report progress', done => {
-    const ffmpeg = spawn(FFMPEG_PATH, args);
+  it('should not include some keys', done => {
+    const ffmpeg = spawn(FFMPEG_PATH, args0);
     const ffmpegProgress = new FFMpegProgress();
     ffmpeg.stderr
       .pipe(ffmpegProgress)
       .on('data', (progress: FFMpegProgressData) => {
-        // console.log(progress);
+        expect(progress).to.not.have.any.keys('progress', 'remaining');
+      });
+    ffmpeg.on('close', code => {
+      done();
+    });
+  });
+
+  it('should be able to report progress', done => {
+    const ffmpeg = spawn(FFMPEG_PATH, args1);
+    const ffmpegProgress = new FFMpegProgress();
+    ffmpegProgress.duration = 10000;
+    ffmpeg.stderr
+      .pipe(ffmpegProgress)
+      .on('data', (progress: FFMpegProgressData) => {
         expect(progress).to.include.keys(
           'bitrate',
           'fps',
@@ -43,27 +66,24 @@ describe('ffmpeg-progress', () => {
     });
   });
   it('should be able to report duration', done => {
-    const ffmpeg = spawn(FFMPEG_PATH, args);
+    const ffmpeg = spawn(FFMPEG_PATH, args1);
     const ffmpegProgress = new FFMpegProgress();
     ffmpeg.stderr
       .pipe(ffmpegProgress)
-      .on('data', (progress: FFMpegProgressData) => {
-      });
+      .on('data', (progress: FFMpegProgressData) => {});
     ffmpeg.on('close', code => {
-      expect(ffmpegProgress.duration).to.be.equal(6890);
+      expect(ffmpegProgress.duration).to.be.equal(10000);
       done();
     });
-
   });
   it('should be able to report error message', done => {
-    const ffmpeg = spawn(FFMPEG_PATH, errargs);
+    const ffmpeg = spawn(FFMPEG_PATH, arg3);
     const ffmpegProgress = new FFMpegProgress();
     ffmpeg.stderr.pipe(ffmpegProgress);
     ffmpeg.on('close', code => {
       expect(ffmpegProgress.exitMessage).to.be.equal(
         'badfile: No such file or directory'
       );
-      // console.log(ffmpegProgress.exitMessage);
       done();
     });
   });
