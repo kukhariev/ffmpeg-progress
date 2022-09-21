6a96e4525f8b33e386f60c23c6809389af163576
kukhariev <q_h@inbox.ru>
2022-09-21 17:27:34 +0300

    chore: update examples

--- config.js
+++ config.js
@@ -4,27 +4,29 @@ const { existsSync } = require('fs');
 const { tmpdir } = require('os');
 
 const tmp = tmpdir();
-const inputFile = 'test/fixtures/input.mp4';
+const inputFile = `${__dirname}/test/fixtures/input.mp4`;
 const outputFile = `${tmp}/output.mp4`;
+const durationSec = 10.0;
 
 const ffmpegPath = String(process.env.FFMPEG_PATH || ffmpegStaticPath);
 
-function generateTestFile(filename, duration = 10) {
+function generateTestFile(filename, durationSec = 10.0) {
   spawnSync(ffmpegPath, [
     '-y',
     '-f',
     'lavfi',
     '-i',
-    `testsrc=duration=${duration}:size=640x360:rate=30`,
+    `testsrc=duration=${durationSec}:size=640x360:rate=30`,
     filename
   ]);
 }
 
 if (!existsSync(inputFile)) {
-  generateTestFile(inputFile, 10);
+  generateTestFile(inputFile, durationSec);
 }
 
 exports.generateTestFile = generateTestFile;
 exports.inputFile = inputFile;
+exports.durationSec = durationSec;
 exports.outputFile = outputFile;
 exports.ffmpegPath = ffmpegPath;
